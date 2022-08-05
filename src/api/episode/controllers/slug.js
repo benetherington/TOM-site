c718f78273b8415ef1822eb59b0b9ac39f21b247
Ben Etherington <ben.j.etherington@gmail.com>
2022-08-05 16:30:52 -0400

    Roll back route test changes (application level routes need to be added by middleware?)

--- src/api/episode/controllers/slug.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    async get(ctx, next) {
-        // called by GET /hello
-        ctx.body = 'Hello World!'; // we could also send a JSON
-    },
-};
--- src/api/episode/routes/episode_slug.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    routes: [
-        {
-            method: 'GET',
-            path: '/:slug([a-z0-9]+(?:-[a-z0-9]+)*)',
-            handler: 'slug.get',
-        },
-    ],
-};
