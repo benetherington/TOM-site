71799e0e49a058a5c40ffbfc6f3634ca2c83a03a
Ben Etherington <ben.j.etherington@gmail.com>
2022-08-21 11:28:27 -0400

    Add some filters for episode views

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,20 @@
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+
+// FILTERS
+const fullTitle = (attributes) =>
+    `Episode ${attributes.ep_num}: ${attributes.title}`;
+const formatNotes = (attributes) => md.render(attributes.show_notes);
+
+// EXPORTS
 module.exports = function (eleventyConfig) {
     // Passthroughs
     eleventyConfig.addPassthroughCopy({'templates/stylesheets': 'stylesheets'});
     eleventyConfig.addPassthroughCopy({'templates/img': 'img'});
+
+    // Filters
+    eleventyConfig.addNunjucksFilter('fullTitle', fullTitle);
+    eleventyConfig.addNunjucksFilter('formatNotes', formatNotes);
 
     // Pptions
     return {
