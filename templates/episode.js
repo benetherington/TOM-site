36b42fbbd84a8d5fc471fcff6fd77d2f42db599a
Ben Etherington <ben.j.etherington@gmail.com>
2022-09-12 10:27:36 -0400

    Work on attachment presentation

--- templates/episode.js
+++ templates/episode.js
@@ -1,5 +1,18 @@
 let masonry;
 
+const resizeAttachment = ({target}) => {
+    const attachment = target.closest('.attachment');
+    attachment.classList.toggle('expanded');
+    masonry.layout();
+};
+
+const addEventListeners = () => {
+    const attchs = document.querySelectorAll('.attachment');
+    attchs.forEach((attch) => {
+        attch.addEventListener('click', resizeAttachment);
+    });
+};
+
 window.addEventListener('load', () => {
     masonry = new Masonry('#attachments', {
         itemSelector: '.attachment',
