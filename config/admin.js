eb2b697b831dc45220b1ea91d2189dcdeeaa0451
Ben Etherington <ben.j.etherington@gmail.com>
2024-01-13 10:30:05 -0500

    Remove default admin JWT secret

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({env}) => ({
     auth: {
-        secret: env('ADMIN_JWT_SECRET', 'ddbc42c9b9644f7d9c343bc098f5db58'),
+        secret: env('ADMIN_JWT_SECRET'),
     },
     watchIgnoreFiles: [
         '**/templates/**',
